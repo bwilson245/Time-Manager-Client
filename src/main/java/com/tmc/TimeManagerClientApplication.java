d68607eefc86d156003737bd2ce050b61a8cde76
Ben Wilson <95599374+bwilson245@users.noreply.github.com>
2022-06-16 22:55:57 -0500

    initial commit

---
+++ src/main/java/com/tmc/TimeManagerClientApplication.java
@@ -0,0 +1,13 @@
+package com.tmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManagerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeManagerClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmc/TimeManagerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeManagerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
